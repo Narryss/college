777e47b3ec7a84e320db3047d0a0f0f120b861ea
Narryss <alembaev.a@yandex.ru>
2024-09-09 11:29:10 +0300

    homework_7

---
+++ homework_7/DKR.pas
@@ -0,0 +1,28 @@
+﻿Unit DKR;
+uses GraphABC;
+
+procedure Snowflake(x,y,l,u:real; t:integer);
+procedure Snowflake2(var x,y:real; l,u:real; t:integer);
+
+begin
+    Snowflake(x,y,l,u,t);
+    x := x + l*cos(u);
+    y := y - l*sin(u);
+end;
+
+begin
+    if t > 0 then
+    begin
+        l := l/3;
+        Snowflake2(x, y, l, u, t-1);
+        Snowflake2(x, y, l, u+pi/3, t-1);
+        Snowflake2(x, y, l, u-pi/3, t-1);
+        Snowflake2(x, y, l, u, t-1);
+    end
+    else
+        Line(Round(x), Round(y), Round(x+cos(u)*l), Round(y-sin(u)*l))
+end;
+ 
+begin
+  SetWindowSize(1000,600);
+end.
---
+++ homework_7/god.pas
@@ -0,0 +1,39 @@
+﻿uses 
+  GraphABC, DKR;
+var t:integer;
+x1,x2,x3,y1,y2,y3,l:real;
+
+procedure KeyDown(Key: integer);
+begin
+  case Key of 
+    VK_A:(x1,x2,x3):=(x1-5,x2-5,x3-5);
+    VK_D:(x1,x2,x3):=(x1+5,x2+5,x3+5);
+    VK_W:(y1,y2,y3):=(y1-5,y2-5,y3-5);
+    VK_S:(y1,y2,y3):=(y1+5,y2+5,y3+5);
+    VK_Z:t:=t+1;
+    VK_X:t:=t-1;
+    VK_V:(x1,y1,x2,y2,x3,y3,l):=((x1-500)*0.8+500,(y1-300)*0.8+300,(x2-500)*0.8+500,(y2-300)*0.8+300,(x3-500)*0.8+500,(y3-300)*0.8+300,l*0.8);
+    VK_C:(x1,y1,x2,y2,x3,y3,l):=((x1-500)*1.2+500,(y1-300)*1.2+300,(x2-500)*1.2+500,(y2-300)*1.2+300,(x3-500)*1.2+500,(y3-300)*1.2+300,l*1.2);
+  end;
+  
+  window.Clear;
+  
+  Snowflake(x1, y1, l, pi/3, t);
+  Snowflake(x2, y2, l, pi, t);
+  Snowflake(x3, y3, l, -pi/3, t);
+  
+end;
+begin
+  t:=3;
+  x1:=10;
+  x2:=410;
+  x3:=210;
+  y1:=354;
+  y2:=354;
+  y3:=8;
+  l:=400;
+  Snowflake(x1, y1, l, pi/3, t);
+  Snowflake(x2, y2, l, pi, t);
+  Snowflake(x3, y3, l, -pi/3, t);
+  OnKeyDown := KeyDown; 
+end.
